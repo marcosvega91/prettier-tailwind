0688828c4829b1e152583ae3e0d6ac7118bfb740
marcosvega91 <marcomoretti0103@gmail.com>
2020-07-15 18:08:08 +0200

    refactor: remove useless file

--- .lintstagerc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	'**/*.js': (files) => [`eslint --quiet --fix ${files.join(' ')}`, `jest --passWithNoTests`],
-	'**/*.{md,js,json,yml,html,css,pcss}': (files) => [`prettier --write ${files.join(' ')}`]
-}

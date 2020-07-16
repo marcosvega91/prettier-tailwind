15fcdd2827f4cbe492e2971be813077a921c6b5f
Marco Moretti <marcomoretti0103@gmail.com>
2020-07-16 22:18:15 +0200

    feat: vue integration (#5)
    * feat: add vue support
    
    fix #4
    
    * doc: add quote

---
+++ test/mocks/vue/default_behaviour_formatted.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="xl:flex items-center py-12 -mx-6">
+		<div class="md:text-center xl:text-left md:max-w-3xl max-w-2xl px-6 mx-auto text-left">
+			<slot />
+		</div>
+		<div class="xl:mt-0 md:max-w-3xl flex-shrink-0 max-w-2xl px-6 mt-12">
+			<slot name="figure" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'prettier-tailwind'
+}
+</script>
---
+++ test/mocks/vue/no_remove_duplicate_formatted.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="xl:flex xl:flex xl:flex items-center py-12 -mx-6"></div>
+</template>
+
+<script>
+export default {
+	name: 'prettier-tailwind'
+}
+</script>

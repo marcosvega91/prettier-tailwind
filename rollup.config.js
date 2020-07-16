edeef2650041adc32ba83fdf3f05974c576c2c10
marcosvega91 <marcomoretti0103@gmail.com>
2020-07-16 10:06:05 +0200

    chore: update build script to copy json files

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,21 @@
 import babel from '@rollup/plugin-babel'
+import copy from 'rollup-plugin-copy'
 
 export default {
 	external: ['prettier/parser-html', 'prettier/parser-flow', 'prettier/parser-babel'],
 	input: 'src/index.js',
 	output: {
-		file: './index.js',
+		file: './dist/index.js',
 		format: 'cjs'
 	},
 	sourceMap: 'inline',
 	plugins: [
+		copy({
+			targets: [
+				{ src: 'src/sorter.json', dest: 'dist/' },
+				{ src: 'src/regex.json', dest: 'dist/' }
+			]
+		}),
 		babel({
 			exclude: 'node_modules/**',
 			presets: [['@babel/preset-env', { modules: false }]]
